9b621f525e490f43f35e43e01f86f33e8e001a5e
TranVuHoang <tranvuhoangjr@gmail.com>
2026-05-31 14:05:10 +0700

    #7 Type Inference

---
+++ projects/lesson7/lesson7.js
@@ -0,0 +1,5 @@
+let count = 10;
+count = "Eric";
+console.log(">>> check count: ", count);
+
+let courses = ["HTML", "CSS", "JS", 96];
---
+++ projects/lesson7/lesson7.ts
@@ -0,0 +1,8 @@
+// gán mặc định type cho vòng đời của biến đó
+let count7TS: string = "Hoàng IT";
+console.log(">>> check count7TS: ", count7TS);
+
+// TS tự động đoán kiểu type
+let courses = ["HTML", "CSS", "JS", 96];
+courses.push(25);
+console.log(">>> courses: ", courses);
