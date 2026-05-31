ded8c619d58f71c9796aa95c58e850782e149382
TranVuHoang <tranvuhoangjr@gmail.com>
2026-05-31 13:50:46 +0700

    #6 Type Annotations

---
+++ projects/lesson6/lesson6.js
@@ -0,0 +1,6 @@
+let count = 1; // type - number
+count = "name"; // type - string
+console.log(">>> check cout:", count);
+
+let names = ["Hoàng", "Trần", "Typescript"]; // array chỉ bao gồm string
+names.push(25);
---
+++ projects/lesson6/lesson6.ts
@@ -0,0 +1,9 @@
+// # ví dụ 1
+let countTS: number = 1; // type - number
+// countTS = "name"; // type - string
+console.log(">>> check coutTS:", countTS);
+
+// # ví dụ 2
+let names: string[] = ["Hoàng", "Trần", "Typescript"]; // array chỉ bao gồm string
+names.push("25");
+console.log(">>> check names:", names);
