a0c654a69d0282877d7857b7830d8afca6e13814
TranVuHoang <tranvuhoangjr@gmail.com>
2026-05-31 14:27:19 +0700

    #8. type number trong TS

---
+++ projects/lesson8/lesson8.js
@@ -0,0 +1,2 @@
+let count = 69; // type - number
+let score = 9.6; // type - number
---
+++ projects/lesson8/lesson8.ts
@@ -0,0 +1,4 @@
+let numberTS: number = 9.6; //type - number
+numberTS = 999999999999999999999999999999999999999;
+
+const a1: number = 29;
