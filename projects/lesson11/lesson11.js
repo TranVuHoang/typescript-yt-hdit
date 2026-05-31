9cd70dce6101a66f879c6a21437d11b821bd5dab
TranVuHoang <tranvuhoangjr@gmail.com>
2026-05-31 15:36:10 +0700

    #10. type object trong TS

---
+++ projects/lesson11/lesson11.js
@@ -0,0 +1,6 @@
+let mine = {};
+mine = {
+  name: "Hoàng Trần",
+};
+mine.address = "Hưng Yên";
+console.log(">>> check mine: ", mine);
---
+++ projects/lesson11/lesson11.ts
@@ -0,0 +1,14 @@
+let person: object = {
+  name: "Hoàng Trần",
+};
+
+// Khai báo type và gán giá trị cho biến trong TS
+let pro1: {
+  name: string;
+  age: number;
+} = {
+  name: "Hoàng Trần",
+  age: 29,
+};
+
+// pro1.address = "Hưng Yên";
